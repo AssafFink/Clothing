6d3597cbe28b4049314a03e4e3c0d2e89e650cbf
MyGarbageAccount <Garbage10Github20Account!>
2025-01-28 21:33:12 +0200

    Fix some bugs

--- main.js
+++ main.js
@@ -2,7 +2,15 @@
 
     document.title = "Amazing";
 
-    function f1() {}
-    function f2() {}
+    function f1() {
+        console.log("F1");
+    }
+    
+    function f2() {
+        console.log("F2");
+    }
+
+    f1();
+    f2();
 
 })();
